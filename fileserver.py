87a3d027ec8c6b155bfd43af7da586cfd2922a1c
akashacn <akashacn@icloud.com>
2013-05-20 15:23:52 +0800

    Init
    Init of this repository

---
+++ fileserver.py
@@ -0,0 +1,15 @@
+'''
+Created on 2013-5-1
+
+@author: zpfalpc23
+'''
+
+
+class FileService:
+    def __init__(self):
+        pass
+    
+    
+if __name__ == "__main__":
+    print "File Server is running!"
+    pass
